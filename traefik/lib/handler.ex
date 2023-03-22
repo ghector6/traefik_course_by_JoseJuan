64b8e666b9bf9909e7908a55456c24cb9292917b
Hector Garcia Rosales <ghectormus@gmail.com>
2023-03-22 17:00:03 -0600

    add: new requests with different paths no exisiting routes

--- traefik/lib/handler.ex
+++ traefik/lib/handler.ex
@@ -13,26 +13,25 @@ defmodule Traefik.Handler do
       |> List.first() 
       |> String.split(" ")
 
-    %{method: method, path: path,  response: "Hello Devs"}
+    %{method: method, path: path,  response: ""}
   end
+
 
   def route(conn) do
     route(conn, conn.method, conn.path)
   end
 
   def route(conn, "GET", "/hello") do
-    %{conn | response: "Hello World"}
+    %{conn | response: "Hello person"}
   end
 
-  def route(conn, "GET", "/developers") do
-    %{conn | response: "Hello Making Devs!!🤓"}
+  def route(conn, "GET", "/developers"  ) do
+    %{conn | response: "Hello Making Devs "}
   end
-
-
 
   def format_response(conn) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 200 OK 
     Content-Type: text/html
     Content-Length: #{String.length(conn.response)}
 
@@ -50,6 +49,9 @@ User-Agent: Mybrowser/0.1
 Accept: */*
 
 """
+
+IO.puts(Traefik.Handler.handle(request_1))
+
 request_2 = """
 GET /developers HTTP/1.1
 Host: makingdevs.com
@@ -57,7 +59,26 @@ User-Agent: Mybrowser/0.1
 Accept: */*
 
 """
-IO.puts(Traefik.Handler.handle(request_1))
-IO.puts("#####################")
 IO.puts(Traefik.Handler.handle(request_2))
+
+
+#request_3 = """
+#GET /hello/1 HTTP/1.1
+#Host: makingdevs.com
+#User-Agent: Mybrowser/0.1
+#Accept: */*
+
+#"""
+#IO.puts(Traefik.Handler.handle(request_3))
+
+request_4 = """
+GET /bugme HTTP/1.1
+Host: makingdevs.com
+User-Agent: Mybrowser/0.1
+Accept: */*
+
+"""
+IO.puts(Traefik.Handler.handle(request_4))
+
+
 

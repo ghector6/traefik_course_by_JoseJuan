c4d44c977bf6ff037a492085b4472ad508f076cc
Hector Garcia Rosales <ghectormus@gmail.com>
2023-03-27 20:21:58 -0600

    add. transform param string in map

--- traefik/lib/traefik/parser.ex
+++ traefik/lib/traefik/parser.ex
@@ -1,10 +1,20 @@
 defmodule Traefik.Parser do
   def parse(request) do
-    [top ,  _param_string] = String.split(request, "\n\n")
+    [top , param_string] = String.split(request, "\n\n")
 
     [request_line | _headers] = String.split(top, "\n")
 
     [method, path, _] = String.split(request_line, " ")
+
+    params = String.trim(params_string) |> URI.decode_query()
+
+    %Traefik.Conn{
+      method: method,
+      path: path,
+      response: "",
+      staus: nil,
+      params: params
+    }
 
     %Traefik.Conn{method: method, path: path,  response: "", status: nil}
   end

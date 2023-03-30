833cfcb8fd6eb8c23eaf5edaff1279aed7afa01c
Hector Garcia Rosales <ghectormus@gmail.com>
2023-03-30 16:25:29 -0600

    removing headers from string list

--- traefik/lib/traefik/organization.ex
+++ traefik/lib/traefik/organization.ex
@@ -2,11 +2,18 @@ defmodule Traefik.Organization do
   @csv_file Path.expand("../..", __DIR__)
 
   def list_developer() do
+    devs_stirng =
     @csv_file
     |> Path.join("MOCK_DATA.csv")
     |> File.read!()
+    |> String.trim()
     |> String.split("\n")
+    |>IO.inspect(label: "DEVS STRING")
+    [_, devs] = devs_in_string
+    devs
     |> Enum.map(&String.split(&1, ","))
+    |> Enum.map(&Traefik.Developer.dev_from_list/1)
+
   end
 
 end

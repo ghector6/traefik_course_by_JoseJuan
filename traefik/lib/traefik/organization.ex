8c9c8877da6bbccc0004ba672808b2f7d56dc7ad
Hector Garcia Rosales <ghectormus@gmail.com>
2023-03-30 16:29:35 -0600

    add: fn dev_from_list and fixing typo
    Also fixing the pattern matching failing at line 12

--- traefik/lib/traefik/organization.ex
+++ traefik/lib/traefik/organization.ex
@@ -2,14 +2,15 @@ defmodule Traefik.Organization do
   @csv_file Path.expand("../..", __DIR__)
 
   def list_developer() do
-    devs_stirng =
+    devs_in_string =
     @csv_file
     |> Path.join("MOCK_DATA.csv")
     |> File.read!()
     |> String.trim()
     |> String.split("\n")
-    |>IO.inspect(label: "DEVS STRING")
-    [_, devs] = devs_in_string
+    |> IO.inspect(label: "Devs string")
+    [_ | devs] = devs_in_string
+
     devs
     |> Enum.map(&String.split(&1, ","))
     |> Enum.map(&Traefik.Developer.dev_from_list/1)

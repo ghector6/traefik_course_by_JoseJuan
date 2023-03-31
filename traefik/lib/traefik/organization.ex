2d6b9c84ef1ad44c24437e0cad02328b9aa11daa
Hector Garcia Rosales <ghectormus@gmail.com>
2023-03-30 18:28:29 -0600

    refacto: Kernel fn and refactor of factorial,

--- traefik/lib/traefik/organization.ex
+++ traefik/lib/traefik/organization.ex
@@ -2,16 +2,12 @@ defmodule Traefik.Organization do
   @csv_file Path.expand("../..", __DIR__)
 
   def list_developer() do
-    devs_in_string =
     @csv_file
     |> Path.join("MOCK_DATA.csv")
     |> File.read!()
     |> String.trim()
     |> String.split("\n")
-    |> IO.inspect(label: "Devs string")
-    [_ | devs] = devs_in_string
-
-    devs
+    |> Kernel.tl()
     |> Enum.map(&String.split(&1, ","))
     |> Enum.map(&Traefik.Developer.dev_from_list/1)
 

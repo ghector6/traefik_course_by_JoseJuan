954c88773a51f0baee6eea2d60893b9702a8c98d
Hector Garcia Rosales <ghectormus@gmail.com>
2023-04-10 16:47:22 -0600

    add: json derive and content_type
    needs to practice the worflow

--- traefik/lib/traefik/api/developer_controller.ex
+++ traefik/lib/traefik/api/developer_controller.ex
@@ -2,7 +2,7 @@ defmodule Traefik.Api.DeveloperController do
   alias Traefik.Conn
 
   def index(%Conn{} = conn, _ \\ %{}) do
-    json = Traefik.Organization.list_developerss() |> Jason.encode!()
+    json = Traefik.Organization.list_developers() |> Jason.encode!()
     %Conn{conn | status: 200, response: json, content_type: "application/jsonn"}
   end
 end
